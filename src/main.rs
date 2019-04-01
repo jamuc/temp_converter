beb15744cdcec1861c1e3530e7d95bbaf541c45f
Jason Franklin <jason.franklin@xing.com>
2019-04-01 22:38:18 +0200

    Implement temperature converter

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+  println!("Temperature Converter: Converting Fahrenheit to Celcius since 1885");
+  println!("Please enter the temperature in Fahrenheit");
+
+  let mut input = String::new();
+  io::stdin().read_line(&mut input)
+    .expect("Unable to read input");
+
+  let fahrenheit: f64 = input.trim().parse()
+    .expect("Was unable to parse the number");
+
+  let celcius = convert(&fahrenheit);
+
+  println!("{} Fahrenheit is {} in Celcius", fahrenheit, celcius);
+}
+
+fn convert(fahrenheit: &f64) -> f64 {
+  (fahrenheit - 32.0) / 1.8
 }
